49fb05a1964595610c940421e5f2027603436e12
Rizki-Setiawan <83151014+Rizki-Setiawan@users.noreply.github.com>
2023-07-01 12:04:48 +0700

    Update index.php

--- xml/index.php
+++ xml/index.php
@@ -20,7 +20,7 @@
 <body id="page-top">
 	<nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
 		<div class="container">
-			<a class="navbar-brand js-scroll-trigger" href="#page-top">My Pengolah XML</a>
+			<a class="navbar-brand js-scroll-trigger" href="#page-top">Pengolah XML</a>
 			<button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 				Menu
 				<i class="fas fa-bars"></i>
