01d29b7fd1e054fcbe4c762423a94980470683be
Rizki-Setiawan <17.rizkisetiawan@gmail.com>
2023-09-09 17:33:27 +0700

    update files

--- json/clear_json.php
+++ json/clear_json.php
@@ -1,5 +1,4 @@
-<?php 
-	include 'koneksi.php';
-	mysqli_query($koneksi,"delete from data_json");
-	header("location:index.php");
-?>
+<?php
+include 'koneksi.php';
+mysqli_query($koneksi, "delete from data_json");
+header("location:index.php");
